9c9c040dedc2115f3ea9e9f50e7b578e82e4042d
leevisin <alwaysjoy233@163.com>
2021-06-14 15:01:55 +0800

    Update ftp.c

--- ftp.c
+++ ftp.c
@@ -41,6 +41,9 @@
 
 char* SERVER_IP = "127.0.0.1";
 char buffer[BUFFER_MAX];
+char *username; //用户名
+char *rnfr_name;
+
 int tcp_server();
 void str_trim_crlf(char *str);
 void str_split(const char *str , char *left, char *right, char c);
@@ -93,25 +96,29 @@ int main(int argc, char** argv){
 			
     memset(buffer,0,sizeof(buffer));
     send(client_sockfd, Entername, sizeof(Entername), 0);
-    recv(client_sockfd, buffer, sizeof(buffer),0);
-    printf("Receive username: %s",buffer);
+    // recv(client_sockfd, buffer, sizeof(buffer),0);
+    // printf("Receive username: %s",buffer);
     
-    memset(buffer,0,sizeof(buffer));
-    send(client_sockfd, Enterpass, sizeof(Enterpass), 0);
-    recv(client_sockfd,buffer,sizeof(buffer),0);
+    // memset(buffer,0,sizeof(buffer));
+    // send(client_sockfd, Enterpass, sizeof(Enterpass), 0);
+    // recv(client_sockfd,buffer,sizeof(buffer),0);
 	
-    printf("Receive password: %s", buffer);
-    if(strncmp(buffer,"PASS 123456",11)!=0){
-        memset(buffer,0,sizeof(buffer));
-        send(client_sockfd, Wrongpass, sizeof(Wrongpass), 0);
-    }
+    // printf("Receive password: %s", buffer);
+    // if(strncmp(buffer,"PASS 123456",11)!=0){
+    //     memset(buffer,0,sizeof(buffer));
+    //     send(client_sockfd, Wrongpass, sizeof(Wrongpass), 0);
+    // }
 
-    memset(buffer,0,sizeof(buffer));
-    send(client_sockfd, succ, sizeof(succ), 0);
-    int sys = recv(client_sockfd, buffer, sizeof(buffer),0);
-    if(strncmp(buffer,"SYST",4)==0 || sys<=0){
-        send(client_sockfd, sysback , sizeof(sysback), 0);
-    }
+    // // 切换到home/student目录下
+    // if(chdir("/home/student") == -1)
+    //     exit(-1);
+
+    // memset(buffer,0,sizeof(buffer));
+    // send(client_sockfd, succ, sizeof(succ), 0);
+    // int sys = recv(client_sockfd, buffer, sizeof(buffer),0);
+    // if(strncmp(buffer,"SYST",4)==0 || sys<=0){
+    //     send(client_sockfd, sysback , sizeof(sysback), 0);
+    // }
 
     while(1){
         char com[COMMAND_MAX];//FTP指令
@@ -121,7 +128,6 @@ int main(int argc, char** argv){
         memset(args, 0, sizeof (args));
 
         int len = recv(client_sockfd, buffer, sizeof(buffer),0);
-        printf("from client:%s\n",buffer);
 
         str_trim_crlf(buffer);
         str_split(buffer, com, args, ' ');
@@ -129,37 +135,30 @@ int main(int argc, char** argv){
         // Need to display user IP Action speed&file when transmitting
         printf("Action=%s, Args=%s\n", com, args);
 
-        // if(strncmp(buffer,"QUIT",4)==0 || len<=0){
-		// 	send(client_sockfd, Quitout, sizeof(Quitout), 0);
-		// 	break; 
-		// }
-
 		if (strcmp("USER", com) == 0) {
-			// handle_USER(client_sockfd, args);
+			handle_USER(client_sockfd, args);
 		} else if (strcmp("PASS", com) == 0) {
-			// handle_PASS(client_sockfd, args);
+			handle_PASS(client_sockfd, args);
 		} else if (strcmp("SYST", com) == 0) {
-			// handle_SYST(client_sockfd);
+			handle_SYST(client_sockfd);
 		} else if (strcmp("PWD", com) == 0) {
 			handle_PWD(client_sockfd);
 		} else if (strcmp("PORT", com) == 0) {
-			// handle_PORT(client_sockfd, args);
+			handle_PORT(client_sockfd, args);
 		} else if (strcmp("CWD", com) == 0) {
-			// handle_CWD(client_sockfd, args);
+			handle_CWD(client_sockfd, args);
 		} else if (strcmp("DELE", com) == 0) {
-			// handle_DELE(client_sockfd, args);
+			handle_DELE(client_sockfd, args);
 		} else if (strcmp("LIST", com) == 0) {
 			// handle_LIST(client_sockfd);
 		} else if (strcmp("MKD", com) == 0) {
-			// handle_MKD(client_sockfd, args);
-		} else if (strcmp("RMD", com) == 0) {
-	 		// handle_RMD(client_sockfd, args);
+			handle_MKD(client_sockfd, args);
 		} else if (strcmp("RNFR", com) == 0) {
-	 		// handle_RNFR(client_sockfd, args);
+	 		handle_RNFR(client_sockfd, args);
 		} else if (strcmp("RNTO", com) == 0) {
-	 		// handle_RNTO(client_sockfd, args);
+	 		handle_RNTO(client_sockfd, args);
 		} else if (strcmp("QUIT", com) == 0) {
-			// handle_QUIT(client_sockfd);
+			handle_QUIT(client_sockfd);
 			break;
 		} else if (strcmp("RETR", com) == 0) {
 			// handle_RETR(client_sockfd, args);
@@ -170,10 +169,7 @@ int main(int argc, char** argv){
 		} else if (strcmp("PASV", com) == 0) {
 			// handle_PASV(client_sockfd);
 		} else {
-			char buf[100];
-			strcpy(buf, "500 ");
-			strcat(buf, com);
-			strcat(buf, "cannot be recognized by server\r\n");
+			char buf[] = "Unimplement command.\r\n";
 			send(client_sockfd, buf, sizeof(buf), 0);
 		}
 
@@ -240,14 +236,53 @@ void str_upper(char *str)
     }
 }
 
+void handle_USER(int client_sockfd, char *args){
+    struct passwd *pw;
+    char buf[] = "331 Please specify the password.\r\n";
+    // if((pw = getpwnam(args)) == NULL)
+    // {
+    //     sprintf(buf, "530 Login incorrect.\r\n");
+    //     send(client_sockfd, buf, sizeof(buf), 0);
+    //     return;
+    // }
+
+    if(strncmp(args,"student",7)!=0){
+        sprintf(buf, "530 Login incorrect.\r\n");
+        send(client_sockfd, buf, sizeof(buf), 0);
+    }
+    username = args;
+    send(client_sockfd, buf, sizeof(buf), 0);
+}
+
+void handle_PASS(int client_sockfd, char *args){
+
+    char buf[] = "230 Login successful! Welcome!\r\n";
+
+    if(strncmp(buffer,"PASS 123456",11)!=0){
+        sprintf(buf, "530 Login incorrect.\r\n");
+        send(client_sockfd, buf, sizeof(buf), 0);
+    }
+
+    sprintf(buf, "230 Login successful! Welcome!\r\n ");
+
+    //home---切换到主目录
+    if(chdir("/home/student") == -1)
+        exit(-1);
+
+    send(client_sockfd, buf, sizeof(buf), 0);
+}
+
+void handle_SYST(int client_sockfd){
+    char buf[] = "215 UNIX Type: L8\r\n";
+    send(client_sockfd, buf, sizeof(buf), 0);
+}
 
 void handle_PWD(int client_sockfd){
     char tmp[1024] = {0};
-    char buf[100];
     if(getcwd(tmp, sizeof tmp) == NULL)
     {
         //return值为-1/0，函数进入系统内核，
-        sprintf(buf, "504 get cwd error\n");
+        char buf[] = "504 get cwd error\r\n";
         send(client_sockfd, buf, sizeof(buf), 0);
         return;
     }
@@ -256,3 +291,121 @@ void handle_PWD(int client_sockfd){
     send(client_sockfd, text, sizeof(text), 0);
 }
 
+void handle_CWD(int client_sockfd, char *args){
+    char buf[] = "250 Directory successfully changed.\r\n";
+    if(chdir(args) == -1)
+    {
+        sprintf(buf, "550 Failed to change directory.\r\n");
+        send(client_sockfd, buf, sizeof(buf), 0);
+        return;
+    }
+
+    send(client_sockfd, buf, sizeof(buf), 0);
+}
+
+void handle_PORT(int client_sockfd, char *args){
+    //设置主动工作模式
+    unsigned int v[6] = {0};
+    sscanf(args, "%u,%u,%u,%u,%u,%u", &v[0], &v[1], &v[2], &v[3], &v[4], &v[5]);
+
+    struct sockaddr_in *p_addr;
+    p_addr = (struct sockaddr_in *)malloc(sizeof (struct sockaddr_in));
+    memset(p_addr, 0, sizeof(struct sockaddr_in));
+    p_addr->sin_family = AF_INET;
+
+    char *p = (char*)&p_addr->sin_port;
+    p[0] = v[4];
+    p[1] = v[5];
+
+    p = (char*)&p_addr->sin_addr.s_addr;
+    p[0] = v[0];
+    p[1] = v[1];
+    p[2] = v[2];
+    p[3] = v[3];
+
+    char buf[] = "200 PORT command successful. Consider using PASV.\r\n";
+    send(client_sockfd, buf, sizeof(buf), 0);
+}
+
+void handle_LIST(int client_sockfd){
+    
+}
+
+void handle_MKD(int client_sockfd, char *args){
+    if(mkdir(args, 0777) == -1)
+    {
+        char buf[] = "550 Create directory operation failed.\r\n";
+        send(client_sockfd, buf, sizeof(buf), 0);
+        return;
+    }
+
+    char text[1024] = {0};
+    if(args[0] == '/') //绝对路径
+        snprintf(text, sizeof text, "257 %s created.\r\n", args);
+    else
+    {
+        //char *getcwd(char *buf, size_t size);
+        char tmp[1024] = {0};
+        if(getcwd(tmp, sizeof tmp) == NULL)
+        {
+            exit(-1);
+        }
+        snprintf(text, sizeof text, "257 %s/%s created.\r\n", tmp, args);
+    }
+    send(client_sockfd, text, sizeof(text), 0);
+}
+
+void handle_DELE(int client_sockfd, char *args){
+    char buf[] = "250 Delete operation successful.\r\n";
+    if(unlink(args) == -1)
+    {
+        //550 Delete operation failed.
+        sprintf(buf, "550 Delete operation failed.\r\n");
+        send(client_sockfd, buf, sizeof(buf), 0);
+        return;
+    }
+    //250 Delete operation successful.
+    send(client_sockfd, buf, sizeof(buf), 0);
+
+}
+
+void handle_RNFR(int client_sockfd, char *args){
+    if(rnfr_name) //防止内存泄露
+    {
+        free(rnfr_name);
+        rnfr_name = NULL;
+    }
+    rnfr_name = (char*)malloc(strlen(args)+1);
+    strcpy(rnfr_name, args);
+    //350 Ready for RNTO.
+    char buf[] = "350 Ready for RNTO.\r\n";
+    send(client_sockfd, buf, sizeof(buf), 0);
+}
+
+void handle_RNTO(int client_sockfd, char *args){
+    char buf[] = "250 Rename successful.\r\n";
+    if(rnfr_name == NULL)
+    {
+        //503 RNFR required first.
+        sprintf(buf, "503 RNFR required first.\r\n");
+        send(client_sockfd, buf, sizeof(buf), 0);
+        return;
+    }
+
+    if(rename(rnfr_name, args) == -1)
+    {
+        sprintf(buf, "550 RRename failed.\r\n");
+        send(client_sockfd, buf, sizeof(buf), 0);
+        return;
+    }
+    free(rnfr_name);
+    rnfr_name = NULL;
+
+    //250 Rename successful.
+    send(client_sockfd, buf, sizeof(buf), 0);
+}
+
+void handle_QUIT(int client_sockfd){
+    char buf[] = "221 Goodbye\r\n";
+    send(client_sockfd, buf, sizeof(buf), 0);
+}
